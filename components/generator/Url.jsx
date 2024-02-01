fa5535f56154e5cd4e0f989e547d4eb5dc26aec5
goncalhurtado <gonzalomilano@gmail.com>
2024-01-31 21:29:09 -0300

    added stylesheets

--- components/generator/Url.jsx
+++ components/generator/Url.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { TextInput } from "react-native-paper";
+import { Button } from "react-native-paper";
+import style from "../../style/styleSheet";
 
 const Url = () => {
   const [text, setText] = useState("");
   return (
-    <View style={{ padding: 10 }}>
+    <View style={style.container}>
       <TextInput
+        style={style.input}
         label="Url"
         value={text}
         onChangeText={(text) => setText(text)}
         mode="outlined"
       />
+      <Button
+        mode="contained"
+        onPress={() => console.log("Pressed")}
+        style={style.button}
+      >
+        Generar QR
+      </Button>
     </View>
   );
 };
---
+++ style/styleSheet.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  button: {
+    width: 150,
+    alignSelf: "center",
+  },
+  input: {
+    width: "100%",
+    marginBottom: 10,
+  },
+});
+
+export default style;
