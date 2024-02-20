0306c29372cdd9ff0969e2c385e6e6af25a778fe
goncal hurtado <goncalhurtado@gmail.com>
2024-02-20 17:48:26 -0300

    added style to save and share funct

--- helpers/shareQr.js
+++ helpers/shareQr.js
@@ -1,13 +1,16 @@
 import * as Sharing from "expo-sharing";
 
-export const shareOnWp = async (uri) => {
+export const shareOnWp = async (uri, setLoading) => {
   try {
+    setLoading(true);
     await Sharing.shareAsync(uri, {
       dialogTitle: "Compartir QR",
       mimeType: "image/jpg",
       UTI: "image/jpg",
     });
+    setLoading(false);
   } catch (error) {
+    setLoading(false);
     console.error("Error al compartir", error);
   }
 };
